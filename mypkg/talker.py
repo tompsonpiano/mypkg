45576083c064f251fb6bb4b6d950833ff811e5f4
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-02-05 11:35:02 +0900

    license update

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,3 +1,7 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tomohiro Hayashi s19c1094hn@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
